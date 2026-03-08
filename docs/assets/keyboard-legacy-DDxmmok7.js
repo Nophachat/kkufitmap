f6bb1d0e3db86dcdda6e4437ef6a1d0f98874fe6
Nophachat <Nophachat.n@kkumail.com>
2026-03-08 11:54:45 +0700

    เริ่มใหม่เวอร์ชั่นเเรก

--- docs/assets/keyboard-legacy-DDxmmok7.js
+++ docs/assets/keyboard-legacy-DDxmmok7.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CLrvHYVA.js"],function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-hZ4zlrby.js"],function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
